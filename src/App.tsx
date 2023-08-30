d4a6fcacdb72c2a03c0fee50394093c759cdbc14
Ian Tavares <iantavares2405@gmail.com>
2023-08-30 00:10:29 -0300

    config App

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,32 @@
+import { useState } from 'react'
+
+import { Header } from './components/Header'
+import { PasswordGenerator } from './components/PasswordGenerator'
+
+import { dark } from './styles/themes'
+
+import { Container, CssBaseline, ThemeProvider } from '@mui/material'
+
 function App() {
-  return <h1>Start</h1>
+  const [theme, setTheme] = useState(dark)
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Container
+        sx={{
+          background: theme.palette.background.default,
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <Header setTheme={setTheme} />
+        <PasswordGenerator />
+      </Container>
+    </ThemeProvider>
+  )
 }
 
 export default App
