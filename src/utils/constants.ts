48b0f8fa861014db7cf872c1bb9b2afa3ccd851a
Ian Tavares <iantavares2405@gmail.com>
2023-08-31 17:23:24 -0300

    refactor constants.ts

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,12 +1,14 @@
-export const characters =
+export const CHARACTERS =
   'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%&*()_-+[]{}?/.,<>'
+
+export const INITIAL_CHARACTERS_AMOUNT = 30
 
 export type Range = {
   min: number
   max: number
 }
 
-export const UpperCaseRange = { min: 0, max: 25 }
-export const LowerCaseRange = { min: 26, max: 51 }
-export const numbersRange = { min: 52, max: 61 }
-export const symbolsRange = { min: 62, max: 83 }
+export const UPPER_CASE_RANGE: Range = { min: 0, max: 25 }
+export const LOWER_CASE_RANGE: Range = { min: 26, max: 51 }
+export const NUMBERS_RANGE: Range = { min: 52, max: 61 }
+export const SYMBOLS_RANGE: Range = { min: 62, max: 83 }
